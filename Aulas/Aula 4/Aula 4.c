6498d60af008313987e6a4c97f7bb0c6020f1e8b
matheus emanoel <matheusemanoelgomessouza@hotmail.com>
2024-03-20 20:35:19 -0300

    Execução do exe

--- Aulas/Aula 4/Aula 4.c
+++ Aulas/Aula 4/Aula 4.c
@@ -6,7 +6,7 @@
 typedef float precos;
 
 typedef enum prod{
-    TV, //Recebe 0
+    TV = 10, //Recebe 0
     SOM,//Recebe 1
     CELULAR//recebe 2
 }produto;
@@ -14,8 +14,27 @@ typedef enum prod{
 produto lista_de_produtos[MAX_PROD];
 precos lista_de_precos[MAX_PROD];
 
+void imprimirProduto(produto prod)
+{
+    switch (prod)
+    {
+    case TV:
+        printf("O produto escolhido foi a TV");
+        break;
+    case SOM:
+        printf("O produto escolhido foi o SOM");
+        break;
+    case CELULAR:
+        printf("O produto escolhido foi celular");
+        break;    
+    default:
+        break;
+    }
+};
+
 int main()
 {
     produto escolha_feita = CELULAR;
-    printf("o prod escolhido foi:%i",&)
+    //printf("o prod escolhido foi:%i",escolha_feita);
+    imprimirProduto(escolha_feita);
 }
