b2211f4b8fd2438efeb7e50e84c32fd3d8439790
matheus emanoel <matheusemanoelgomessouza@hotmail.com>
2024-03-20 19:49:09 -0300

    Inicio da aula 4

---
+++ Aulas/Aula 4/Aula 4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_PROD 50
+
+typedef float precos;
+
+typedef enum prod{
+    TV, //Recebe 0
+    SOM,//Recebe 1
+    CELULAR//recebe 2
+}produto;
+
+produto lista_de_produtos[MAX_PROD];
+precos lista_de_precos[MAX_PROD];
+
+int main()
+{
+    produto escolha_feita = CELULAR;
+    printf("o prod escolhido foi:%i",&)
+}
