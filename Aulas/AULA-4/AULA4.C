abc5c9d9f5961423c0ed3bbc728059102ee488a1
matheus.souza <matheus.souza@TI-08>
2024-03-21 10:53:05 -0300

    Organização da pasta

---
+++ Aulas/AULA-4/AULA4.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TAMANHOMAX 90
+
+typedef enum
+{  //0  1      2   3   4
+    TV,SOM,CELULAR,JBL,PC
+}CASASBAHIA;
+
+
+void tela()
+{
+    printf("Selecione o produto que deseja atualizar o preço:\n");
+    for (int i = 0; i <TAMANHOMAX; i++)
+    {
+        
+    }
+    
+}
+int Main()
+{
+
+}
