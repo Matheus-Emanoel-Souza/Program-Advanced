d2d676d5be81708f35995ef4e19bb54f13aca8c6
matheus emanoel <matheusemanoelgomessouza@hotmail.com>
2024-05-15 20:51:48 -0300

    #Atualização. <Começo de um novo programa. CinematicaVeiculo>

--- Aulas/Aula15.05/biblioteca.h
+++ Aulas/Aula15.05/biblioteca.h
@@ -2,4 +2,13 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+typedef struct 
+{
+    char experimento[40];
+    float velocidade;
+    float aceleracao;
+    float distancia;
+    float tempo;
+}cinematicaveiculo;
+
 void MeuPrint(char *msg);
