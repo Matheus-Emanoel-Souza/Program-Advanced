44b707f8e61662abf477e26e7b68379172e235a8
matheus <matheus.souza@ogmoes.com.br>
2024-05-16 14:00:29 -0300

    Alura. <Comecei o joguinho da forca do alura.>

---
+++ Alura/jogo da forca.c
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+
+do
+{
+prinf("
+/****************/
+/ Jogo de Forca */
+/****************/
+
+  _______       
+ |/      |      
+ |           
+ |           
+ |             
+ |            
+ |              
+_|___           
+
+
+_ _ _ _ _ _ _ _ _ 
+Qual letra?");
+scanf("%s",char Letra);
+
+} while (/* condition */);
