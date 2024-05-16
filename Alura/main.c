78a6aed6740c264af02ca4e291ba17a7b62a3dbe
matheus <matheus.souza@ogmoes.com.br>
2024-05-16 14:49:00 -0300

    #Atualização. <Criação dos ponteiros!!>

--- Alura/main.c
+++ Alura/main.c
@@ -14,10 +14,16 @@ int main()
     printf(" |              \n");
     printf("_|___           \n\n");
     printf("_ _ _ _ _ _ _ _ _ \n");
-    printf("Qual letra? ");
+    printf("Qual letra?\n");
 
     char Letra;
-    scanf("%s",&Letra);
+    char *ptr;
+    ptr=&Letra;
+    scanf("%s",&Letra);//%s aponta para uma string.
+    printf("%s\n",&ptr);// o mesmo da regra de cima.
+    printf("%c\n",*ptr);//%c aponta para o caracter apontado.
+    printf("%p\n",ptr);//%p imprime o endereço de memória.
+
  
  //Usar biblioteca do 
     return 0;
