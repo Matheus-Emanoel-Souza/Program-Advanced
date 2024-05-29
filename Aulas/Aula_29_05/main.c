8dfa07c01fce8053edf8d304ef5fb12cf927dbd7
matheus emanoel <matheusemanoelgomessouza@hotmail.com>
2024-05-29 19:29:13 -0300

    Aula de hoje.

---
+++ Aulas/Aula_29_05/main.c
@@ -0,0 +1,16 @@
+//Lista encadeada.
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct livro
+{
+    char titulo[40];
+    char autor[40];
+    int ano;
+}Livro;
+
+int main()
+{
+    Livro infantil = {"Rei leão","Simba",1998};
+    
+}
