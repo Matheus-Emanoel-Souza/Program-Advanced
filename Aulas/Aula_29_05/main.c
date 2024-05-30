0139345f65352b90a8b99ee7a5b15fca5b1e5097
matheus emanoel <matheusemanoelgomessouza@hotmail.com>
2024-05-29 21:14:08 -0300

    Andamento da aula

--- Aulas/Aula_29_05/main.c
+++ Aulas/Aula_29_05/main.c
@@ -9,8 +9,69 @@ typedef struct livro
     int ano;
 }Livro;
 
+typedef struct no
+{
+    Livro info;
+    struct no * proximo;
+}no;
+
+typedef struct lista
+{
+    no*primeiro;
+    no*marcador;
+    int contador;
+}lista;
+
+no criano(Livro book)
+{
+    no NOVO_NO;
+    NOVO_NO.info = book;
+    NOVO_NO.proximo = NULL;
+
+    return NOVO_NO;
+}
+
+lista cria_lista()
+{
+    lista nova_lista={NULL, NULL, 0};
+    return nova_lista;
+}
+
+void add_na_lista(no* no, lista* lista)
+{
+if (lista->contador == 0)
+{
+    lista->primeiro=no;
+    lista->marcador = no;
+}else
+{
+    lista->marcador->proximo = no;
+}
+
+
+lista->contador ++;
+}
+
+
+void imprimirLivro(Livro book)
+{
+    printf("Autor: %s\n",book.autor);
+    printf("Titulo: %s\n",book.titulo);
+    printf("Ano: %i\n",book.ano);
+}
+
 int main()
 {
     Livro infantil = {"Rei leão","Simba",1998};
-    
+    Livro outro_livro = {"Branca de neve","anos",1900};
+
+    lista lista_de_livros = cria_lista();
+    no novo_no = criano(infantil);
+    no no_renovado = criano(outro_livro);
+    add_na_lista(&novo_no, &lista_de_livros);
+    novo_no = criano(outro_livro);
+    add_na_lista(&no_renovado,&lista_de_livros);
+
+    imprimirLivro(infantil);
+
 }
